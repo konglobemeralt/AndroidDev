82061599d4da4bdfaf2e4088ef8a384c2c297aa7
konglobemeralt <jesper.blidkvist@live.se>
2017-06-21 15:16:30 +0200

    Added crime class

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Jesper on 2017-06-21.
+ */
+
+public class Crime {
+
+    private UUID mID;
+    private String mTitle;
+
+    public Crime(){
+        // Generate unique identifier
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
