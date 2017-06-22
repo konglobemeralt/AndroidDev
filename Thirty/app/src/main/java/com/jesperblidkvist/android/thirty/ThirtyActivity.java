7a1d64d751971a37f0f3e1af132206cda3b8fd95
konglobemeralt <jesper.blidkvist@live.se>
2017-06-22 15:11:02 +0200

    Finished for today, dices working as planned

--- Thirty/app/src/main/java/com/jesperblidkvist/android/thirty/ThirtyActivity.java
+++ Thirty/app/src/main/java/com/jesperblidkvist/android/thirty/ThirtyActivity.java
@@ -1,11 +1,13 @@
 package com.jesperblidkvist.android.thirty;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.jesperblidkvist.android.thirty.model.ThirtyGame;
 
@@ -51,7 +53,12 @@ public class ThirtyActivity extends AppCompatActivity {
                 }
                 else{
                     //TODO: Display a toast with some text like "Please unselect at least one dice"
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please unselect at least one dice!";
+                    int duration = Toast.LENGTH_SHORT;
 
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
 
                 //Log.d("ThirtyActivity", game.toString());
