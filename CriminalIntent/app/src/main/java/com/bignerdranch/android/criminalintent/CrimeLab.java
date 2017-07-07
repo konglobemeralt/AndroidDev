49270dcc4a1ea825f5be74cba4dd4bd29736b970
konglobemeralt <jesper.blidkvist@live.se>
2017-07-07 14:19:49 +0200

    Added list for crimes in crimelab

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -3,12 +3,17 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Jesper on 2017-07-07.
  */
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -19,5 +24,18 @@ public class CrimeLab {
 
     private CrimeLab(Context context){
 
-    };
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime crime: mCrimes){
+            if(crime.getID().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
 }
