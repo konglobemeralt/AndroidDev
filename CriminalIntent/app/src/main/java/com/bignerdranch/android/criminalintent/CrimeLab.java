30667a48f5a4599229a7a88e7494266484851252
konglobemeralt <jesper.blidkvist@live.se>
2017-07-07 14:22:33 +0200

    Constructor creates 100 dummy crime objects to put in list

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,7 +24,15 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
 
+        //DUMMY CRIMES
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Every other one
+            mCrimes.add(crime);
+        }
     }
 
     public List<Crime> getCrimes() {
