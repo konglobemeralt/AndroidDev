6d6d47ca78ecddfbba2f79de4b11c06c5f8f0ef8
konglobemeralt <jesper.blidkvist@live.se>
2017-07-12 16:17:09 +0200

    Stopping on page 209 for now

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,6 +10,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Jesper on 2017-07-12.
@@ -15,14 +18,23 @@ import java.util.List;
 
 public class CrimePagerActivity extends FragmentActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime.id";
+
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
 
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
 
