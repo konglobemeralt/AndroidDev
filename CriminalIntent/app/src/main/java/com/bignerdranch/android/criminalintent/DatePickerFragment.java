5a3ad341c471f3c7242d721a5089dbb9f7b61b97
konglobemeralt <jesper.blidkvist@live.se>
2017-07-22 16:23:01 +0200

    Added date picker fragment

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -13,5 +13,8 @@ public class DatePickerFragment extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         return new AlertDialog.Builder(getActivity())
-                .setTitle(R.string.date_picker_title)    }
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
 }
