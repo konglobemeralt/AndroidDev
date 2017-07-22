ac30084bd899475e447f3191843407f7fc4c8c40
konglobemeralt <jesper.blidkvist@live.se>
2017-07-22 16:19:37 +0200

    Added strings

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by Jesper on 2017-07-22.
+ */
+
+public class DatePickerFragment extends DialogFragment{
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)    }
+}
