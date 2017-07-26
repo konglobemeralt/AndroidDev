ea5e8d4217f749e6d77fdb79476ce6a95bcd83e9
konglobemeralt <jesper.blidkvist@live.se>
2017-07-26 14:16:16 +0200

    Email app design

--- EmailAppMockup/app/src/main/java/com/jesperblidkvist/android/emailappmockup/MainMailActivity.java
+++ EmailAppMockup/app/src/main/java/com/jesperblidkvist/android/emailappmockup/MainMailActivity.java
@@ -1,13 +1,25 @@
 package com.jesperblidkvist.android.emailappmockup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainMailActivity extends AppCompatActivity {
-
+public class MainMailActivity extends ListActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_mail);
+
+        // storing string resources into Array
+        String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main_mail, R.id.label, adobe_products));
+
     }
 }
