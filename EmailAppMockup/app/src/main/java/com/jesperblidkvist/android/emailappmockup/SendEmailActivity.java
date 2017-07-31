c795aad18f4c4b094be41d5d8b67040aabbfb504
konglobemeralt <jesper.blidkvist@live.se>
2017-07-31 15:08:07 +0200

    Toolbar now appears everywhere, forgot to change Activity to AppCompat in SendEmailAct...

--- EmailAppMockup/app/src/main/java/com/jesperblidkvist/android/emailappmockup/SendEmailActivity.java
+++ EmailAppMockup/app/src/main/java/com/jesperblidkvist/android/emailappmockup/SendEmailActivity.java
@@ -2,6 +2,9 @@ package com.jesperblidkvist.android.emailappmockup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,12 +12,19 @@ import android.widget.EditText;
  * Created by Jesper on 2017-07-31.
  */
 
-public class SendEmailActivity extends Activity {
+public class SendEmailActivity extends AppCompatActivity {
 
     Button buttonSend;
     EditText textTo;
     EditText textSubject;
     EditText textMessage;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.email_main, menu);
+        return true;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
