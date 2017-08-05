96fa6c0fb8a4740e2d4b36fe0d97f042a32e6989
konglobemeralt <jesper.blidkvist@live.se>
2017-08-05 22:00:52 +0200

    Casting dbCursor to CrimeCursorWrapper

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -36,8 +36,19 @@ public class CrimeLab {
     }
 
     public Crime getCrime(UUID id) {
-
-        return null;
+        CrimeCursorWrapper cursor = (CrimeCursorWrapper) queryCrimes(
+                CrimeDbSchema.CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
+        }
     }
 
     private static ContentValues getContentValues(Crime crime){
