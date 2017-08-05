f921215fd5112a816d4210e0f2c067adf1017d27
konglobemeralt <jesper.blidkvist@live.se>
2017-08-05 21:50:37 +0200

    bugfix, legit commit

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -9,8 +9,13 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
 
+
     public Crime() {
-        mId = UUID.randomUUID();
+        this(UUID.randomUUID());
+    }
+
+    public Crime(UUID id) {
+        mId = id;
         mDate = new Date();
     }
 
