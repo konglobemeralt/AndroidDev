946f2a12a6701e0a87c7f7d4b35f9afd3bdec13a
konglobemeralt <jesper.blidkvist@live.se>
2017-08-17 09:51:34 +0200

    Now calling constructor

--- AndroidLab-Phase-1/app/src/main/java/com/jesperblidkvist/android/androidlab_phase_1/DetailActivity.java
+++ AndroidLab-Phase-1/app/src/main/java/com/jesperblidkvist/android/androidlab_phase_1/DetailActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
 
+    SimpleBookManager mSimpleBookManger;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mSimpleBookManger = new SimpleBookManager();
+
+
     }
 }
