7b5a24afcfadcf1fb2bb3553022aca8fae021293
konglobemeralt <jesper.blidkvist@live.se>
2017-08-20 18:49:30 +0200

    working on adding functionality for the rest

--- AndroidLab-Phase-1/app/src/main/java/com/jesperblidkvist/android/androidlab_phase_1/SummaryActivity.java
+++ AndroidLab-Phase-1/app/src/main/java/com/jesperblidkvist/android/androidlab_phase_1/SummaryActivity.java
@@ -2,12 +2,37 @@ package com.jesperblidkvist.android.androidlab_phase_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    SimpleBookManager mSimpleBookManger;
+
+    TextView mTotalPrice;
+    TextView mMostExpensive;
+    TextView mLeastExpensive;
+    TextView mAveragePrice;
+    TextView mBookCounterString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        mSimpleBookManger = new SimpleBookManager();
+
+        mTotalPrice = (TextView) findViewById(R.id.averagePriceString);
+        mTotalPrice.setText(mSimpleBookManger.getTotalCost());
+
+        mMostExpensive = (TextView) findViewById(R.id.mostExpensiveItemString);
+        mMostExpensive.setText(mSimpleBookManger.getMaxPrice());
+
+        mLeastExpensive = (TextView) findViewById(R.id.leastExpensiveItemString);
+        mLeastExpensive.setText(mSimpleBookManger.getMinPrice());
+
+
     }
 }
